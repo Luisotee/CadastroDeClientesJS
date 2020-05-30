d2cdb45d0293db55bcfd7e6787d9569ae25db98d
Luis Otavio <luisotaviomgr1@gmail.com>
2020-05-30 10:43:03 -0300

    Changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,26 @@
+import * as funcoes from '../scripts/teste.js'
+
+console.log(funcoes.soma(5, 4));
 document.getElementById('send').addEventListener("click", send);
 
 function send() {
-    const nome = document.querySelector(".name").value;
-    const email = document.querySelector(".email").value;
-    const cell = document.getElementById("celular").value;
-    const cep = document.querySelector(".cep").value;
-    console.log(cell);
+    const usuario = {
+        nome: document.querySelector(".name").value,
+        celular: document.getElementById("celular").value,
+        email: document.querySelector(".email").value,
+        endereco: {
+            cep: document.getElementById("cep").value,
+            logradouro: document.querySelector(".street").value,
+            numero: document.querySelector(".number").value,
+            bairro: document.querySelector(".address").value,
+            cidade: document.querySelector(".city").value,
+            estado: document.getElementById("uf").value,
+        },
+    };
+
+    console.log(usuario);
+}
+
+function saveToLocal() {
+    localStorage.setItem('data', JSON.stringify(usuario));
 }
