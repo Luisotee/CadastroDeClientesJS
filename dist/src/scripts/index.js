85d36ba4acd8268794f6efbabb18ce3073dbf415
Luis Otavio <luisotaviomgr1@gmail.com>
2020-05-31 10:32:22 -0300

    Builded code

---
+++ dist/src/scripts/index.js
@@ -0,0 +1,30 @@
+import * as utils from './validateCadastro';
+
+document.getElementById('send').addEventListener("click", send);
+
+const users = JSON.parse(localStorage.getItem('users')) || [];
+
+function send() {
+    if (utils.validateForm()) {
+        const user = {
+            nome: document.querySelector(".name").value,
+            celular: document.getElementById("celular").value,
+            email: document.querySelector(".email").value,
+            endereco: {
+                cep: document.getElementById("cep").value,
+                logradouro: document.querySelector(".street").value,
+                numero: document.querySelector(".number").value,
+                bairro: document.querySelector(".address").value,
+                cidade: document.querySelector(".city").value,
+                estado: document.getElementById("uf").value,
+            },
+        };
+        users.push(user);
+        saveToLocal()
+    }
+}
+
+
+function saveToLocal() {
+    localStorage.setItem('users', JSON.stringify(users));
+}
---
+++ dist/src/scripts/listItens.js
@@ -0,0 +1,60 @@
+var listElement = document.querySelector('#list ul');
+
+const users = JSON.parse(localStorage.getItem('users')) || [];
+
+function renderUsers() {
+    listElement.innerHTML = '';
+
+    for (user of users) {
+        const userElement = document.createElement('li');
+        const buttonElement = document.createElement('button');
+
+        buttonElement.setAttribute('class', 'excludeButton');
+
+        var pos = users.indexOf(user);
+
+        const userName = document.createTextNode("Nome: " + user.nome);
+        userElement.appendChild(userName);
+        userElement.appendChild(document.createElement("br"));
+        const userCell = document.createTextNode("Celular: " + user.celular);
+        userElement.appendChild(userCell);
+        userElement.appendChild(document.createElement("br"));
+        const userEmail = document.createTextNode("Email: " + user.email);
+        userElement.appendChild(userEmail);
+        userElement.appendChild(document.createElement("br"));
+        const userCep = document.createTextNode("CEP: " + user.endereco.cep);
+        userElement.appendChild(userCep);
+        userElement.appendChild(document.createElement("br"));
+        const userStreet = document.createTextNode("Logradouro: " + user.endereco.logradouro);
+        userElement.appendChild(userStreet);
+        userElement.appendChild(document.createElement("br"));
+        const userNumber = document.createTextNode("Numero: " + user.endereco.numero);
+        userElement.appendChild(userNumber);
+        userElement.appendChild(document.createElement("br"));
+        const userBairro = document.createTextNode("Bairro: " + user.endereco.bairro);
+        userElement.appendChild(userBairro);
+        userElement.appendChild(document.createElement("br"));
+        const userCity = document.createTextNode("Cidade: " + user.endereco.cidade);
+        userElement.appendChild(userCity);
+        userElement.appendChild(document.createElement("br"));
+        const userState = document.createTextNode("Estado: " + user.endereco.estado);
+        userElement.appendChild(userState);
+
+        listElement.appendChild(userElement);
+
+        buttonElement.setAttribute('onclick', 'deleteUser(' + pos + ')')
+
+        const buttonText = document.createTextNode('Excluir');
+        buttonElement.appendChild(buttonText);
+
+        listElement.appendChild(buttonElement);
+    }
+}
+
+function deleteUser(pos) {
+    users.splice(pos, 1);
+    renderUsers();
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+renderUsers();
